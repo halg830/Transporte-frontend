92c9c14d2f0946904831dd2684f0ade7e2ae1081
elblanquito <johanblancocata@gmail.com>
2023-10-08 22:52:42 -0500

    quasar v1

--- src/components/conductores.vue
+++ src/components/conductores.vue
@@ -1,487 +1,189 @@
 
-<script setup>
-$(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();
-	
-	// Select/Deselect checkboxes
-	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
-		if(this.checked){
-			checkbox.each(function(){
-				this.checked = true;                        
-			});
-		} else{
-			checkbox.each(function(){
-				this.checked = false;                        
-			});
-		} 
-	});
-	checkbox.click(function(){
-		if(!this.checked){
-			$("#selectAll").prop("checked", false);
-		}
-	});
-});
+<script>
+import axios from "axios";
+import { ref } from "vue";
+
+const columns = ref([
+  {
+    name: "Nombre",
+    label: "Nombre",
+    align: "left",
+    field: (row) => row.nombre,
+  },
+  {
+    name: "Cedula",
+    label: "Cedula",
+    field: (row) => row.cedula,
+  },
+  {
+    name: "Email",
+    label: "Email",
+    field: (row) => row.email,
+  },
+  {
+    name: "Estado",
+    label: "Estado",
+    field: (row) => row.estado,
+  },
+  {
+    name: "Opciones",
+    label: "Opciones",
+    field: "actions",
+  },
+]);
+
+const rows = ref([]);
+const id=ref("")
+const nombre = ref("");
+const cedula = ref("");
+const email = ref("");
+const estado = ref(1)
+const toolbar = ref(false);
+const cambiar = ref(false);
+
+const agregarcliente = async () => {
+  
+
+  if (cambiar.value == true) {
+    const data = {
+    id: id.value,
+    nombre: nombre.value,
+  };
+    const buscar = rows.value.findIndex(r=>r._id==id.value)
+    
+    console.log(data);
+    const cliente = await axios.put(
+      `https://transporte-el2a.onrender.com/api/cliente/editar`,
+      data
+    ).then((response)=>{
+      console.log("r", response);
+      rows.value.splice(buscar, 1,response.data.cliente)
+    }).catch((error)=>{
+      console.log("e", error);
+    })
+
+    
+  } else {
+    const data = {
+    nombre: nombre.value,
+    cedula: cedula.value,
+    email: email.value,
+  };
+    const cliente = await axios.post(
+      "https://transporte-el2a.onrender.com/api/cliente/guardar",
+      data
+    );
+
+    rows.value.push(cliente.data.cliente);
+  }
+
+  toolbar.value = false;
+};
+
+async function obtenerClientes() {
+  console.log("Esperando datos");
+  const clientes = await axios.get(
+    "https://transporte-el2a.onrender.com/api/cliente/all"
+  );
+
+  rows.value = clientes.data.cliente;
+}
+obtenerClientes();
+
+const editar = (row) => {
+  console.log(row);
+  toolbar.value = true;
+  id.value=row._id
+  cambiar.value=true
+  nombre.value=row.nombre
+  cedula.value=row.cedula
+  email.value=row.email
+  estado.value=row.estado
+};
+
+export default {
+  setup() {
+    return {
+      toolbar,
+      selected: ref([]),
+      columns,
+      rows,
+      id,
+      nombre,
+      cedula,
+      email,
+      estado,
+      agregarcliente,
+      editar,
+    };
+  },
+};
 </script>
 
-
-
 <template>
-  <div class="cont">====================tabla conductor=====================</div>
-  <title>Bootstrap CRUD Data Table for Database with Modal Form</title>
+	<div>
+		<q-btn label="Añadir" color="primary" @click="toolbar = true" />
+		<q-dialog v-model="toolbar">
+			<q-card>
+				<q-toolbar>
+					<q-avatar>
+						<img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg" />
+					</q-avatar>
 
+					<q-toolbar-title>Agregar cliente</q-toolbar-title>
 
-<div class="container-xl">
-	<div class="table-responsive">
-		<div class="table-wrapper">
-			<div class="table-title">
-				<div class="row">
-					<div class="col-sm-6">
-						<h2>Regitro <b>Conductores</b></h2>
-					</div>
-					<div class="col-sm-6">
-						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Agregar nuevo Conductor </span></a>
-						<a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Eliminar</span></a>						
-					</div>
-				</div>
-			</div>
-			<table class="table table-striped table-hover">
+					<q-btn flat round dense icon="close" v-close-popup />
+				</q-toolbar>
+
+				<q-card-section>
+					<label for="">Nombre: </label><br />
+					<input type="text" v-model="nombre" />
+					<br />
+					<label for="">Cedula: </label><br />
+					<input type="number" v-model="cedula" />
+					<br />
+					<label for="">Email: </label><br />
+					<input type="text" v-model="email" /><br />
+					<label for="">Estado: </label><br />
+					<input type="number" v-model="estado" /><br />
+					<button @click="agregarcliente()">Enviar</button>
+				</q-card-section>
+			</q-card>
+		</q-dialog>
+
+		<div class="q-pa-md">
+			<q-markup-table>
 				<thead>
 					<tr>
 						<th>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="selectAll">
-								<label for="selectAll"></label>
-							</span>
+							<h4 class="q-ma-xs text-left">Clientes</h4>
 						</th>
-						<th>Nombre</th>
-						<th>Email</th>
-						<th>Cedula</th>
-						<th>Telefono</th>
-						<th>Actions</th>
+					</tr>
+					<tr>
+						<th class="text-left"><b>Nombre</b></th>
+						<th class="text-right"><b>Cedula</b></th>
+						<th class="text-right"><b>Email</b></th>
+						<th class="text-right"><b>Estado</b></th>
+						<th class="text-right"><b>Opciones</b></th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="checkbox1" name="options[]" value="1">
-								<label for="checkbox1"></label>
-							</span>
-						</td>
-						<td>Thomas Hardy</td>
-						<td>thomashardy@mail.com</td>
-						<td>12313849606</td>
-						<td>(171) 555-2222</td>
-						<td>
-							<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+					<tr v-for="row in rows">
+						<td class="text-left">{{ row.nombre }}</td>
+						<td class="text-right">{{ row.cedula }}</td>
+						<td class="text-right">{{ row.email }}</td>
+						<td class="text-right">{{ row.estado }}</td>
+						<td class="text-right">
+							<q-btn label="Editar" color="primary" @click="editar(row)" />
 						</td>
 					</tr>
-					<tr>
-						<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="checkbox2" name="options[]" value="1">
-								<label for="checkbox2"></label>
-							</span>
-						</td>
-						<td>Dominique Perrier</td>
-						<td>dominiqueperrier@mail.com</td>
-						<td>12313849606</td>
-						<td>(313) 555-5735</td>
-						<td>
-							<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="checkbox3" name="options[]" value="1">
-								<label for="checkbox3"></label>
-							</span>
-						</td>
-						<td>Maria Anders</td>
-						<td>mariaanders@mail.com</td>
-						<td>12313849606</td>
-						<td>(503) 555-9931</td>
-						<td>
-							<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="checkbox4" name="options[]" value="1">
-								<label for="checkbox4"></label>
-							</span>
-						</td>
-						<td>Fran Wilson</td>
-						<td>franwilson@mail.com</td>
-						<td>12313849606</td>
-						<td>(204) 619-5731</td>
-						<td>
-							<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr>					
-					<tr>
-						<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="checkbox5" name="options[]" value="1">
-								<label for="checkbox5"></label>
-							</span>
-						</td>
-						<td>Martin Blank</td>
-						<td>martinblank@mail.com</td>
-						<td>12313849606</td>
-						<td>(480) 631-2097</td>
-						<td>
-							<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-						</td>
-					</tr> 
 				</tbody>
-			</table>
-			<div class="clearfix">
-				<div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
-				<ul class="pagination">
-					<li class="page-item disabled"><a href="#">Previous</a></li>
-					<li class="page-item"><a href="#" class="page-link">1</a></li>
-					<li class="page-item"><a href="#" class="page-link">2</a></li>
-					<li class="page-item active"><a href="#" class="page-link">3</a></li>
-					<li class="page-item"><a href="#" class="page-link">4</a></li>
-					<li class="page-item"><a href="#" class="page-link">5</a></li>
-					<li class="page-item"><a href="#" class="page-link">Next</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>        
-</div>
-<!-- Edit Modal HTML -->
-<div id="addEmployeeModal" class="modal fade">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<form>
-				<div class="modal-header">						
-					<h4 class="modal-title">Add Employee</h4>
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				</div>
-				<div class="modal-body">					
-					<div class="form-group">
-						<label>Name</label>
-						<input type="text" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label>Email</label>
-						<input type="email" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label>Address</label>
-						<textarea class="form-control" required></textarea>
-					</div>
-					<div class="form-group">
-						<label>Phone</label>
-						<input type="text" class="form-control" required>
-					</div>					
-				</div>
-				<div class="modal-footer">
-					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-					<input type="submit" class="btn btn-success" value="Add">
-				</div>
-			</form>
+			</q-markup-table>
 		</div>
 	</div>
-</div>
-<!-- Edit Modal HTML -->
-<div id="editEmployeeModal" class="modal fade">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<form>
-				<div class="modal-header">						
-					<h4 class="modal-title">Editar Conductor</h4>
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				</div>
-				<div class="modal-body">					
-					<div class="form-group">
-						<label>Nombre</label>
-						<input type="text" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label>Email</label>
-						<input type="email" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label>Cedula</label>
-						<textarea class="form-control" required></textarea>
-					</div>
-					<div class="form-group">
-						<label>Telefono</label>
-						<input type="text" class="form-control" required>
-					</div>					
-				</div>
-				<div class="modal-footer">
-					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-					<input type="submit" class="btn btn-info" value="Save">
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
-<!-- Delete Modal HTML -->
-<div id="deleteEmployeeModal" class="modal fade">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<form>
-				<div class="modal-header">						
-					<h4 class="modal-title">Eliminar Conductor</h4>
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				</div>
-				<div class="modal-body">					
-					<p>¿Está seguro de que desea eliminar estos registros?</p>
-					<p class="text-warning"><small>Esta acción no se puede deshacer.</small></p>
-				</div>
-				<div class="modal-footer">
-					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-					<input type="submit" class="btn btn-danger" value="Eliminar">
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
 </template>
-
-<!-- scoped sirve para evitar que los estilos afecte a los
-demas componentes  -->
 <style scoped>
-.cont {
-  color: rgb(187, 69, 255);
-}
-
-body {
-	color: #566787;
-	background: #f5f5f5;
-	font-family: 'Varela Round', sans-serif;
-	font-size: 13px;
-}
-.table-responsive {
-    margin: 30px 0;
-}
-.table-wrapper {
-	background: #fff;
-	padding: 20px 25px;
-	border-radius: 3px;
-	min-width: 1000px;
-	box-shadow: 0 1px 1px rgba(0,0,0,.05);
-}
-.table-title {        
-	padding-bottom: 15px;
-	background: #435d7d;
-	color: #fff;
-	padding: 16px 30px;
-	min-width: 100%;
-	margin: -20px -25px 10px;
-	border-radius: 3px 3px 0 0;
-}
-.table-title h2 {
-	margin: 5px 0 0;
-	font-size: 24px;
-}
-.table-title .btn-group {
-	float: right;
-}
-.table-title .btn {
-	color: #fff;
-	float: right;
-	font-size: 13px;
-	border: none;
-	min-width: 50px;
-	border-radius: 2px;
-	border: none;
-	outline: none !important;
-	margin-left: 10px;
-}
-.table-title .btn i {
-	float: left;
-	font-size: 21px;
-	margin-right: 5px;
-}
-.table-title .btn span {
-	float: left;
-	margin-top: 2px;
-}
-table.table tr th, table.table tr td {
-	border-color: #e9e9e9;
-	padding: 12px 15px;
-	vertical-align: middle;
-}
-table.table tr th:first-child {
-	width: 60px;
-}
-table.table tr th:last-child {
-	width: 100px;
-}
-table.table-striped tbody tr:nth-of-type(odd) {
-	background-color: #fcfcfc;
-}
-table.table-striped.table-hover tbody tr:hover {
-	background: #f5f5f5;
-}
-table.table th i {
-	font-size: 13px;
-	margin: 0 5px;
-	cursor: pointer;
-}	
-table.table td:last-child i {
-	opacity: 0.9;
-	font-size: 22px;
-	margin: 0 5px;
-}
-table.table td a {
-	font-weight: bold;
-	color: #566787;
-	display: inline-block;
-	text-decoration: none;
-	outline: none !important;
-}
-table.table td a:hover {
-	color: #2196F3;
-}
-table.table td a.edit {
-	color: #FFC107;
-}
-table.table td a.delete {
-	color: #F44336;
-}
-table.table td i {
-	font-size: 19px;
-}
-table.table .avatar {
-	border-radius: 50%;
-	vertical-align: middle;
-	margin-right: 10px;
-}
-.pagination {
-	float: right;
-	margin: 0 0 5px;
-}
-.pagination li a {
-	border: none;
-	font-size: 13px;
-	min-width: 30px;
-	min-height: 30px;
-	color: #999;
-	margin: 0 2px;
-	line-height: 30px;
-	border-radius: 2px !important;
+*{
 	text-align: center;
-	padding: 0 6px;
 }
-.pagination li a:hover {
-	color: #666;
-}	
-.pagination li.active a, .pagination li.active a.page-link {
-	background: #03A9F4;
-}
-.pagination li.active a:hover {        
-	background: #0397d6;
-}
-.pagination li.disabled i {
-	color: #ccc;
-}
-.pagination li i {
-	font-size: 16px;
-	padding-top: 6px
-}
-.hint-text {
-	float: left;
-	margin-top: 10px;
-	font-size: 13px;
-}    
-/* Custom checkbox */
-.custom-checkbox {
-	position: relative;
-}
-.custom-checkbox input[type="checkbox"] {    
-	opacity: 0;
-	position: absolute;
-	margin: 5px 0 0 3px;
-	z-index: 9;
-}
-.custom-checkbox label:before{
-	width: 18px;
-	height: 18px;
-}
-.custom-checkbox label:before {
-	content: '';
-	margin-right: 10px;
-	display: inline-block;
-	vertical-align: text-top;
-	background: white;
-	border: 1px solid #bbb;
-	border-radius: 2px;
-	box-sizing: border-box;
-	z-index: 2;
-}
-.custom-checkbox input[type="checkbox"]:checked + label:after {
-	content: '';
-	position: absolute;
-	left: 6px;
-	top: 3px;
-	width: 6px;
-	height: 11px;
-	border: solid #000;
-	border-width: 0 3px 3px 0;
-	transform: inherit;
-	z-index: 3;
-	transform: rotateZ(45deg);
-}
-.custom-checkbox input[type="checkbox"]:checked + label:before {
-	border-color: #03A9F4;
-	background: #03A9F4;
-}
-.custom-checkbox input[type="checkbox"]:checked + label:after {
-	border-color: #fff;
-}
-.custom-checkbox input[type="checkbox"]:disabled + label:before {
-	color: #b8b8b8;
-	cursor: auto;
-	box-shadow: none;
-	background: #ddd;
-}
-/* Modal styles */
-.modal .modal-dialog {
-	max-width: 400px;
-}
-.modal .modal-header, .modal .modal-body, .modal .modal-footer {
-	padding: 20px 30px;
-}
-.modal .modal-content {
-	border-radius: 3px;
-	font-size: 14px;
-}
-.modal .modal-footer {
-	background: #ecf0f1;
-	border-radius: 0 0 3px 3px;
-}
-.modal .modal-title {
-	display: inline-block;
-}
-.modal .form-control {
-	border-radius: 2px;
-	box-shadow: none;
-	border-color: #dddddd;
-}
-.modal textarea.form-control {
-	resize: vertical;
-}
-.modal .btn {
-	border-radius: 2px;
-	min-width: 100px;
-}	
-.modal form label {
-	font-weight: normal;
-}	
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import App from './App.vue'
 import { createApp } from 'vue'
 import './style.css'
+
 import { router } from "./routes/routes.js";
+import { Quasar, Dialog } from 'quasar';
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Quasar, {
+    plugins:{Dialog}
+})
 
 app.mount('#app')
